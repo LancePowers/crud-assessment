c1948dfb10211ef02694cb8dffef6c2b0b276936
Lance <Lance@thelpstory.com>
2015-09-09 16:00:38 -0600

    added image

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,31 +5,44 @@ var Vegan = mongoose.model('vegans');
 
 // get all vegans
 router.get('/vegans', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Vegan.find(function(err,vegans){
+    res.json(vegans);
+  })
 });
 
 //post all vegans
 router.post('/vegans', function(req, res, next) {
-  console.log(req.body)
   new Vegan(req.body)
-  .save(function(req){
-    res.json({message:req});
+  .save(function(err, vegan){
+    res.json({message:'success'});
   })
 });
 
 //get one vegan
 router.get('/vegan/:id', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var query = {"_id": req.params.id};
+  console.log(req.params.id)
+  Vegan.findOne(query,function(err,vegan){
+    res.json(vegan);
+  });
 });
 
 // put one vegan
 router.put('/vegan/:id', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var query = {"_id": req.params.id};
+  var update = req.body;
+  var options ={new: true};
+  Vegan.findOneAndUpdate(query,update,options,function(err,vegan){
+    res.json(vegan);
+  })
 });
 
 // delete one vegan
 router.delete('/vegan/:id', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var query = {"_id": req.params.id};
+  Vegan.findOneAndRemove(query,function(err,vegan){
+    res.json(vegan);
+  })
 });
 
 module.exports = router;
